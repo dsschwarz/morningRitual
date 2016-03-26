6ae426a210927ecccc7a96ef03775da0415c621c
Dan Schwarz <dan-schwarz@hotmail.com>
2016-03-26 19:33:01 -0400

    Create user related pages

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,25 @@ function getRoutes(roomService) {
         res.render("index", {title: "Lobby List", lobbies: lobbies, errors: req.flash("error")});
     });
 
+    router.get("/login", function (req, res, next) {
+        res.render("login");
+    });
+
+    router.post("/login", function (req, res, next) {
+        var params = req.body;
+
+        res.send(params);
+    });
+    router.get("/register", function (req, res, next) {
+        res.render("register");
+    });
+
+    router.post("/register", function (req, res, next) {
+        var params = req.body;
+
+        res.send(params);
+    });
+
     return router;
 }
 
